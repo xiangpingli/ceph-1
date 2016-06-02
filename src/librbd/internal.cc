ae64cd37167ba637c5c7011bc3cf514a743da8b7
Dongsheng Yang <dongsheng.yang@easystack.cn>
2016-06-02 04:32:15 -0400

    librbd/clone: set RBD_FEATURE_STRIPINGV2 in feature if stripe related options specified
    Signed-off-by: Dongsheng Yang <dongsheng.yang@easystack.cn>

--- src/librbd/internal.cc
+++ src/librbd/internal.cc
@@ -1470,6 +1470,7 @@ int mirror_image_disable_internal(ImageCtx *ictx, bool force,
     uint64_t order;
     uint64_t size;
     uint64_t p_features;
+    uint64_t stripe_unit, stripe_count;
     int partial_r;
     librbd::NoOpProgressContext no_op;
     ImageCtx *c_imctx = NULL;
@@ -1504,7 +1505,7 @@ int mirror_image_disable_internal(ImageCtx *ictx, bool force,
     }
 
     if (use_p_features) {
-      c_opts.set(RBD_IMAGE_OPTION_FEATURES, p_features);
+      features = p_features;
     }
 
     order = p_imctx->order;
@@ -1512,6 +1513,12 @@ int mirror_image_disable_internal(ImageCtx *ictx, bool force,
       c_opts.set(RBD_IMAGE_OPTION_ORDER, order);
     }
 
+    if (c_opts.get(RBD_IMAGE_OPTION_STRIPE_UNIT, &stripe_unit) == 0 ||
+	c_opts.get(RBD_IMAGE_OPTION_STRIPE_COUNT, &stripe_count) == 0) {
+      features |= RBD_FEATURE_STRIPINGV2;
+    }
+
+    c_opts.set(RBD_IMAGE_OPTION_FEATURES, features);
     r = create(c_ioctx, c_name, size, c_opts, non_primary_global_image_id,
                primary_mirror_uuid);
     if (r < 0) {
